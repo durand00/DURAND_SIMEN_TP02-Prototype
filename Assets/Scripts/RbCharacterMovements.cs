a195f38d1ff3fa4016e6b0a360e22954c513b44f
durand00 <durantsimen@gmail.com>
2021-03-20 13:41:29 -0400

    Premier Commit
    Premier Commit

---
+++ Assets/Scripts/RbCharacterMovements.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RbCharacterMovements : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public float jumpHeight = 1f;
+
+    // Transform de la position des pieds
+    public Transform feetPosition;
+
+    private float inputVertical;
+    private float inputHorizontal;    
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assigner le Rigidbody
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vérifier si l'on touche le sol
+        isGrounded = Physics.CheckSphere(feetPosition.position, 0.15f, 1, QueryTriggerInteraction.Ignore);
+
+        // Vérifier les inputs du joueur
+        // Vertical (W, S et Joystick avant/arrière)
+        inputVertical = Input.GetAxis("Vertical");
+        // Horizontal (A, D et Joystick gauche/droite)
+        inputHorizontal = Input.GetAxis("Horizontal");
+
+        // Vecteur de mouvements (Avant/arrière + Gauche/Droite)
+        moveDirection = transform.forward * inputVertical + transform.right * inputHorizontal;  
+        
+        // Sauter
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Déplacer le personnage selon le vecteur de direction
+        rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+    }
+}
